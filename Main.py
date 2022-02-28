ea277fa9d3b2403b2f90127d5632b33360823ef1
David Lima <67931753+davidolima@users.noreply.github.com>
2022-02-28 10:40:18 -0300

    V1.1

--- Main.py
+++ Main.py
@@ -2,117 +2,111 @@ from PIL import Image
 import random, os
 import pygame as pg
 
-def limparTela ():
-    screen.fill((255,255,255))
-    pg.display.update()
-
 def gerarRGBAleatorio():
     return (random.randrange(1,256),random.randrange(1,256),random.randrange(1,256))
 
 def salvarImagem (diretorio, nome):
     pg.image.save(screen,f"{diretorio}/{nome}.png")
 
-def mudarCor(imagem, cor1, cor2):
-    pa = pg.PixelArray(imagem)
-    pa.replace(cor1,cor2,0.5)
-    return imagem
-
 def mudarCor(imagem, novaCor):
     imagem.fill(novaCor[0:3] + (255,), None, pg.BLEND_RGB_MULT)
     return imagem
 
-def gerarTartarugas(num):
-    global cascoNum, olhoNum, peleNum, fundoNum, chaoNum
-    limparTela()
+def gerarImagens(num):
+    global camada_4_num,camada_3_num, camada_6_num, camada_5_num, camada_1_num, camada_2_num
     numeroDeImagens = 0
 
     for i in range(num):
-        fundoNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada1')])+1)
-        chaoNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada2')])+1)
-        cascoNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada3')])+1)
-        peleNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada5')])+1)
-        olhoNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada6')])+1)
+        screen.fill((255,255,255))
+        camada_1_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada1')])+1)
+        camada_2_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada2')])+1)
+        camada_3_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada3')])+1)
+        camada_4_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada4')])+1)
+        camada_5_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada5')])+1)
+        camada_6_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada6')])+1)
         
-        acessorioRostoNum = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio1/")])+1)
-        acessorioCorpoNum = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio2/")])+1)
-        acessorioPesNum = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio3/")])+1)         
+        camada_7_1_num = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio1/")])+1)
+        camada_7_2_num = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio2/")])+1)
+        camada_7_3_num = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio3/")])+1)         
 
-        gerarTartarugaAleatoria(cascoNum,olhoNum,peleNum,fundoNum,chaoNum,acessorioRostoNum,acessorioCorpoNum,acessorioPesNum)
+        gerarImagemAleatoria(camada_1_num,camada_2_num,camada_3_num,camada_4_num,\
+                             camada_5_num,camada_6_num,camada_7_1_num,camada_7_2_num,\
+                             camada_7_3_num)
         
         pg.display.update()
-        salvarImagem("Out", (cascoNum,olhoNum,peleNum,fundoNum,chaoNum))
+        salvarImagem("Out", identidade)
         numeroDeImagens += 1
 
     print(numeroDeImagens, 'imagens criadas.')
 
-def gerarTartarugaAleatoria(cascoNum, olhoNum, peleNum, fundoNum, chaoNum,acessorioRostoNum,acessorioCorpoNum,acessorioPesNum):
+def gerarImagemAleatoria(camada_1_num, camada_2_num, camada_3_num, camada_4_num, camada_5_num, camada_6_num,camada_7_1_num,camada_7_2_num,camada_7_3_num):
     global identidade
-    #Carregar Imagens com o PIL
-    fundoPIL = Image.open(f'./Imagens/Camada1/{fundoNum}.png').convert('RGBA')
-    chaoPIL = Image.open(f'./Imagens/Camada2/{chaoNum}.png').convert('RGBA')
-    cascoPIL = Image.open(f'./Imagens/Camada3/{cascoNum}.png').convert('RGBA')
-    basePIL = Image.open('./Imagens/Camada4/BASE.png').convert('RGBA')
-    pelePIL = Image.open(f'./Imagens/Camada5/{peleNum}.png').convert('RGBA')
-    olhoPIL = Image.open(f'./Imagens/Camada6/{olhoNum}.png').convert('RGBA')
 
-    acessorioRostoPIL = Image.open(f'./Imagens/Camada7/Acessorio1/{acessorioRostoNum}.png').convert('RGBA')
-    acessorioCorpoPIL = Image.open(f'./Imagens/Camada7/Acessorio2/{acessorioCorpoNum}.png').convert('RGBA')
-    acessorioPesPIL = Image.open(f'./Imagens/Camada7/Acessorio3/{acessorioPesNum}.png').convert('RGBA')
+    screen.fill((255,255,255))
+
+    #Carregar Imagens com o PIL
+    camada_1_pil = Image.open(f'./Imagens/Camada1/{camada_1_num}.png').convert('RGBA')
+    camada_2_pil = Image.open(f'./Imagens/Camada2/{camada_2_num}.png').convert('RGBA')
+    camada_3_pil = Image.open(f'./Imagens/Camada3/{camada_3_num}.png').convert('RGBA')
+    camada_4_pil = Image.open(f'./Imagens/Camada4/{camada_4_num}.png').convert('RGBA')
+    camada_5_pil = Image.open(f'./Imagens/Camada5/{camada_5_num}.png').convert('RGBA')
+    camada_6_pil = Image.open(f'./Imagens/Camada6/{camada_6_num}.png').convert('RGBA')
+
+    camada_7_1_pil = Image.open(f'./Imagens/Camada7/Acessorio1/{camada_7_1_num}.png').convert('RGBA')
+    camada_7_2_pil = Image.open(f'./Imagens/Camada7/Acessorio2/{camada_7_2_num}.png').convert('RGBA')
+    camada_7_3_pil = Image.open(f'./Imagens/Camada7/Acessorio3/{camada_7_3_num}.png').convert('RGBA')
 
     #Converter as imagens para Surfaces do Pygame
-    base = pg.image.fromstring(basePIL.tobytes(),basePIL.size,basePIL.mode)
-    casco = pg.image.fromstring(cascoPIL.tobytes(),cascoPIL.size,cascoPIL.mode)
-    olho = pg.image.fromstring(olhoPIL.tobytes(),olhoPIL.size,olhoPIL.mode)
-    pele = pg.image.fromstring(pelePIL.tobytes(),pelePIL.size,pelePIL.mode)
-    fundo = pg.image.fromstring(fundoPIL.tobytes(),fundoPIL.size,fundoPIL.mode)
-    chao = pg.image.fromstring(chaoPIL.tobytes(),chaoPIL.size,chaoPIL.mode)
+    camada_1 = pg.image.fromstring(camada_1_pil.tobytes(),camada_1_pil.size,camada_1_pil.mode)
+    camada_2 = pg.image.fromstring(camada_2_pil.tobytes(),camada_2_pil.size,camada_2_pil.mode)
+    camada_3 = pg.image.fromstring(camada_3_pil.tobytes(),camada_3_pil.size,camada_3_pil.mode)
+    camada_4 = pg.image.fromstring(camada_4_pil.tobytes(),camada_4_pil.size,camada_4_pil.mode)
+    camada_5 = pg.image.fromstring(camada_5_pil.tobytes(),camada_5_pil.size,camada_5_pil.mode)
+    camada_6 = pg.image.fromstring(camada_6_pil.tobytes(),camada_6_pil.size,camada_6_pil.mode)
 
-    acessorioRosto = pg.image.fromstring(acessorioRostoPIL.tobytes(),acessorioRostoPIL.size,acessorioRostoPIL.mode)
-    acessorioCorpo = pg.image.fromstring(acessorioCorpoPIL.tobytes(),acessorioCorpoPIL.size,acessorioCorpoPIL.mode)
-    acessorioPes = pg.image.fromstring(acessorioPesPIL.tobytes(),acessorioPesPIL.size,acessorioPesPIL.mode)
-
+    camada_7_1 = pg.image.fromstring(camada_7_1_pil.tobytes(),camada_7_1_pil.size,camada_7_1_pil.mode)
+    camada_7_2 = pg.image.fromstring(camada_7_2_pil.tobytes(),camada_7_2_pil.size,camada_7_2_pil.mode)
+    camada_7_3 = pg.image.fromstring(camada_7_3_pil.tobytes(),camada_7_3_pil.size,camada_7_3_pil.mode)
 
     #Mudar cor das imagens
-    corFundo = gerarRGBAleatorio()
-    corChao = gerarRGBAleatorio()
+    camada_1_cor = gerarRGBAleatorio()
+    camada_2_cor = gerarRGBAleatorio()
 
-    corCasco = gerarRGBAleatorio()
-    corPele = gerarRGBAleatorio()
+    camada_3_cor = gerarRGBAleatorio()
+    camada_5_cor = gerarRGBAleatorio()
 
-    corAcessorioRosto =gerarRGBAleatorio()
-    corAcessorioCorpo = gerarRGBAleatorio()
-    corAcessorioPes = gerarRGBAleatorio()
+    camada_7_1_cor =gerarRGBAleatorio()
+    camada_7_2_cor = gerarRGBAleatorio()
+    camada_7_3_cor = gerarRGBAleatorio()
     
-    fundo = mudarCor(fundo, corFundo)
-    chao = mudarCor(chao, corChao)
+    camada_1 = mudarCor(camada_1, camada_1_cor)
+    camada_2 = mudarCor(camada_2, camada_2_cor)
 
-    casco = mudarCor(casco, corCasco)
-    pele = mudarCor(pele, corPele)
+    camada_3 = mudarCor(camada_3, camada_3_cor)
+    camada_5 = mudarCor(camada_5, camada_5_cor)
 
-    acessorioRosto = mudarCor(acessorioRosto, corAcessorioRosto)
-    acessorioCorpo = mudarCor(acessorioCorpo, corAcessorioCorpo)
-    acessorioPes = mudarCor(acessorioPes, corAcessorioPes)
+    camada_7_1 = mudarCor(camada_7_1, camada_7_1_cor)
+    camada_7_2 = mudarCor(camada_7_2, camada_7_2_cor)
+    camada_7_3 = mudarCor(camada_7_3, camada_7_3_cor)
 
-    #          Background --> Foreground
-    camadas = [fundo, chao, casco, base, pele, olho, acessorioRosto, acessorioCorpo, acessorioPes]
+    #          Background                                   -->                                    Foreground
+    camadas = [camada_1, camada_2, camada_3, camada_4, camada_5, camada_6, camada_7_1, camada_7_2, camada_7_3]
 
     for camada in camadas:
         camada = pg.transform.scale(camada, (tela_width,tela_heigth))
         screen.blit(camada, (0,0))
 
     #Gerar identidade
-    identidade = str(cascoNum)+str(olhoNum)+str(peleNum)+str(fundoNum)+str(chaoNum)\
-               + str(corAcessorioCorpo[0])+str(corAcessorioCorpo[1])+str(corAcessorioCorpo[2])\
-               + str(corAcessorioPes[0])+str(corAcessorioPes[1])+str(corAcessorioPes[2])\
-               + str(corAcessorioRosto[0])+str(corAcessorioRosto[1])+str(corAcessorioRosto[2])\
-               + str(corCasco[0])+str(corCasco[1])+str(corCasco[2])\
-               + str(corChao[0])+str(corChao[1])+str(corChao[2])\
-               + str(corFundo[0])+str(corFundo[1])+str(corFundo[2])\
-               + str(corPele[0])+str(corPele[1])+str(corPele[2])
+    identidade = str(camada_1_num)+str(camada_2_num)+str(camada_2_num)+str(camada_3_num)+str(camada_4_num)+str(camada_5_num) + str(camada_6_num)\
+               + str(camada_7_2_cor[0])+str(camada_7_2_cor[1])+str(camada_7_2_cor[2])\
+               + str(camada_7_3_cor[0])+str(camada_7_3_cor[1])+str(camada_7_3_cor[2])\
+               + str(camada_7_1_cor[0])+str(camada_7_1_cor[1])+str(camada_7_1_cor[2])\
+               + str(camada_3_cor[0])+str(camada_3_cor[1])+str(camada_3_cor[2])\
+               + str(camada_2_cor[0])+str(camada_2_cor[1])+str(camada_2_cor[2])\
+               + str(camada_1_cor[0])+str(camada_1_cor[1])+str(camada_1_cor[2])\
+               + str(camada_5_cor[0])+str(camada_5_cor[1])+str(camada_5_cor[2])
 
     pg.display.update()
-
-
 
 def Main ():
     global tela_heigth, tela_width, screen
@@ -124,7 +118,7 @@ def Main ():
     pg.display.set_caption("Gerador de NFTs")
     pg.display.init()
     pg.init()
-    limparTela()
+    screen.fill((255,255,255))
     running = True
 
     while running:
@@ -134,23 +128,25 @@ def Main ():
 
             elif event.type == pg.KEYDOWN:
                 if pg.key.name(event.key) == 'return':
-                    fundoNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada1')])+1)
-                    chaoNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada2')])+1)
-                    cascoNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada3')])+1)
-                    peleNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada5')])+1)
-                    olhoNum = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada6')])+1)
+                    camada_1_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada1')])+1)
+                    camada_2_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada2')])+1)
+                    camada_3_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada3')])+1)
+                    camada_4_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada4')])+1)
+                    camada_5_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada5')])+1)
+                    camada_6_num = random.randrange(1,len([name for name in os.listdir('./Imagens/Camada6')])+1)
                     
-                    acessorioRostoNum = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio1/")])+1)
-                    acessorioCorpoNum = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio2/")])+1)
-                    acessorioPesNum = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio3/")])+1)
+                    camada_7_1_num = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio1/")])+1)
+                    camada_7_2_num = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio2/")])+1)
+                    camada_7_3_num = random.randrange(1,len([name for name in os.listdir("./Imagens/Camada7/Acessorio3/")])+1)
 
-                    
-                    gerarTartarugaAleatoria(cascoNum,olhoNum,peleNum,fundoNum,chaoNum,acessorioRostoNum,acessorioCorpoNum,acessorioPesNum)
+                    gerarImagemAleatoria(camada_1_num,camada_2_num,camada_3_num,camada_4_num,\
+                                         camada_5_num,camada_6_num,camada_7_1_num,camada_7_2_num,\
+                                         camada_7_3_num)
 
                 elif pg.key.name(event.key) == 'backspace':
-                    gerarTartarugas(int(input("Gerar quantas imagens?")))
+                    gerarImagens(int(input("Gerar quantas imagens?")))
                 elif pg.key.name(event.key) == 's':
                     salvarImagem("Out", identidade)
                 elif pg.key.name(event.key) == 'i':
-                    print(f"Casco: {cascoNum}\nOlho: {olhoNum}\nPele: {peleNum}\nFundo: {fundoNum}\nChao: {chaoNum}\nAC Cabeça: {acessorioRostoNum}\nAC Pés: {acessorioPesNum}\nAC Corpo: {acessorioCorpoNum}\n")
+                    print(f"\n----------------------\nID: {identidade}\nCamada 1: {camada_1_num}\nCamada 2: {camada_2_num}\nCamada 3: {camada_3_num}\nCamada 4: {camada_4_num}\nCamada 5: {camada_5_num}\nCamada 6: {camada_6_num}\nCamada 7 AC 1: {camada_7_1_num}\nCamada 7 AC 2: {camada_7_2_num}\nCamada 7 AC 3: {camada_7_3_num}\n")
 Main()
